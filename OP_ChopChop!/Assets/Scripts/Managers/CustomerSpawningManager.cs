8ddd5b3e91249a36ef171a5913764235e42740a7
AJ <angelitojoseph.astilla@benilde.edu.ph>
2025-01-30 00:16:17 +0800

    Worked on Customer Spawning/Order
    - Implemented the Canvas panel where the Customer's order will spawn in
       - used the old order UI as a placeholder
    - Added customer movement
       - Customer moves from their spawn point to their chairs
       - NOTE: not yet fixed, still need to troubleshoot it

--- OP_ChopChop!/Assets/Scripts/Managers/CustomerSpawningManager.cs
+++ OP_ChopChop!/Assets/Scripts/Managers/CustomerSpawningManager.cs
@@ -6,17 +6,27 @@ public class CustomerSpawningManager : Singleton<CustomerSpawningManager>
 {
     protected override void Awake() { base.Awake(); }
 
+    [Header("Arrays")]
     [SerializeField] private GameObject[] customerSpawnPoints;
+    [SerializeField] private Transform[] customerSeatingPoints;
+   
+
+    [Header("CustomerVariable")]
     [SerializeField] private GameObject[] customerModelPrefab; //This is the prefab for the Customer itself
+    [SerializeField] private float customerSpeed;
+
+    [Header("Variable Counts")]
     [SerializeField] private int maxCustomerToSpawn;
     [SerializeField] private int currentCustomerCount;
 
+    [Header("Timer")]
     [SerializeField] private float nextCustomerTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(ITimerForNextCustomerSpawn()); //Pls put this under a condition once testing is done
+        customerSpeed = customerSpeed * Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -38,9 +48,11 @@ public class CustomerSpawningManager : Singleton<CustomerSpawningManager>
         {
             if(!customerSpawnPoints[i].GetComponent<SpawnLocationScript>()._IsPrefabPresent)
             {
-                GameObject createCustomer = Instantiate(customerModelPrefab[0], 
+                GameObject createdCustomer = Instantiate(customerModelPrefab[0], 
                                                         customerSpawnPoints[i].transform.position, 
                                                         Quaternion.identity);
+                    
+                   
 
                 currentCustomerCount++;
 
@@ -51,6 +63,21 @@ public class CustomerSpawningManager : Singleton<CustomerSpawningManager>
 
         StartCoroutine(ITimerForNextCustomerSpawn());
     }
+
+    /*
+    private void CustomerMoveToSeat(GameObject createdCustomer)
+    {
+        customerSpeed = customerSpeed * Time.deltaTime;
+
+        for (int x = 0; x < customerSeatingPoints.Length; x++)
+        {
+           
+        }
+
+        createdCustomer.GetComponent<CustomerOrder>().SpawnCustomerOrder();
+        
+    }
+    */
 
     public bool IsEmptySpawnLocation()
     {
