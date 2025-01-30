fb81c0aeacf5537bfd6f9a6c2ebe66a5fe964654
Mauhay <12107793@benilde.edu.ph>
2025-01-30 11:12:17 +0800

    Script Updates
    - Completed Dynamic Trashable.cs
    - Moved Scripts to certain folders for better organization
    - Created new MainGameScene

--- OP_ChopChop!/Assets/Scripts/Actors/Trashable.cs
+++ OP_ChopChop!/Assets/Scripts/Actors/Trashable.cs
@@ -6,17 +6,5 @@ public enum TrashTypes { Food, Ingredients, Equipment };
 
 public class Trashable : MonoBehaviour
 {
-    [SerializeField] TrashTypes _trashTypes;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TrashTypes _trashTypes;
 }
