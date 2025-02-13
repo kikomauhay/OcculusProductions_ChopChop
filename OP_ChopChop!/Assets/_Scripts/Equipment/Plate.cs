267683d1d422667aa342b8320615a7e31975b6d2
Kiko <kikomauhay@yahoo.com>
2025-02-14 04:17:18 +0800

    Script Updates
    - Fleshed Out CleanManager.cs
    - Made changes and added comments regarding plate.cs
    - Handwashing.cs edited, check comments

--- OP_ChopChop!/Assets/_Scripts/Equipment/Plate.cs
+++ OP_ChopChop!/Assets/_Scripts/Equipment/Plate.cs
@@ -1,10 +1,11 @@
-using UnityEngine;
+ using UnityEngine;
 
 public class Plate : Equipment
 {
     public bool IsDirty => _isDirty;
     
     [SerializeField] Material _dirtyPlateMat, _cleanPlateMat;
+    [SerializeField] Collider _cleanTrigger;
     [SerializeField] bool _isDirty;
 
     protected override void Start()
@@ -13,6 +14,7 @@ public class Plate : Equipment
 
         name = "Plate";
         _isDirty = false;
+        _cleanTrigger.enabled = false;
 
         GetComponent<MeshRenderer>().material = _cleanPlateMat;
     }
@@ -21,8 +23,30 @@ public class Plate : Equipment
     {
         if (Input.GetKeyUp(KeyCode.Space)) 
             TogglePlateSanitation(); // test
+
+        if (_cleanTrigger == null) return;
+
+        DishWash();
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        Sponge sponge = other.GetComponent<Sponge>();
+        Rigidbody _spongeRb = sponge.GetComponent<Rigidbody>();
+        //Create a formula that tracks the change in velocity and compares it to a certain value
+        //If velocity is greater than assigned value, start coroutine for cleaning
+        //suggestion ko lang coroutine but if there's a more efficient way in cleaning the plate please don't hesitate to try it out
+        //If you do go the coroutine route, I think you'd need to put the checker to avoid having the coroutine run again and again.       
+    }
+
+    private void DishWash()
+    {
+        if(_isDirty)
+            _cleanTrigger.enabled = true;
+
+        else _cleanTrigger.enabled = false;
+    }
+    #region Public Functions
     public void TogglePlateSanitation() 
     {
         _isDirty = !_isDirty;
@@ -42,4 +66,5 @@ public class Plate : Equipment
         _isDirty = false;
         GetComponent<MeshRenderer>().material = _cleanPlateMat;
     }
+    #endregion
 }
