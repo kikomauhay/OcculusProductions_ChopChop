2e4789cf70ad5c9dd60c054b3eba7a478d234d78
Isagani Coel <coelfactora@gmail.com>
2025-02-25 23:00:32 +0800

    GaniProg 0.13.1
    - implemented random customer face

--- OP_ChopChop!/Assets/_Scripts/Actors/Customer/CustomerAppearance.cs
+++ OP_ChopChop!/Assets/_Scripts/Actors/Customer/CustomerAppearance.cs
@@ -1,22 +1,55 @@
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 [RequireComponent(typeof(CustomerOrder), typeof(CustomerActions))]
 public class CustomerAppearance : MonoBehaviour 
 {     
 
-#region Customer_Variants
+#region Customer_Variant_Members
 
     [Header("Customer Material Renderers")]
-    [SerializeField] MeshRenderer _ears; 
-    [SerializeField] MeshRenderer _tail, _body;
+    [SerializeField] SpriteRenderer _face;
+    [SerializeField] MeshRenderer _ears, _tail, _body;
 
     [Tooltip("0 = Calico, 1 = Siamese, 2 = Tabby, 3 = Torbie, 4 = Tuxedo")] 
     [SerializeField] SkinVariant[] _skinVariants;
+
+    [SerializeField] FaceVariant _faceVariant;
+    [SerializeField] Sprite[] _faces; // test
+
 
 #endregion
 
     void Awake()
     {        
+        int i = Random.Range(0, _skinVariants.Length);
+
+        _body.material = _skinVariants[i].BodyMaterial;
+        _ears.material = _skinVariants[i].EarVariants[Random.Range(0, _skinVariants[i].EarVariants.Length)];
+        _tail.material = _skinVariants[i].TailVariants[Random.Range(0, _skinVariants[i].TailVariants.Length)];
+
+        _face.sprite = _faceVariant.NeutralFace;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            _face.sprite = null;
+            _face.sprite = _faces[Random.Range(0, _faces.Length)];
+
+            ResetSkin();
+
+            Debug.LogWarning("Randomized customer face!");
+        }
+    }
+
+    void ResetSkin()
+    {
+        _body.material = null;
+        _ears.material = null;
+        _tail.material = null;
+
         int i = Random.Range(0, _skinVariants.Length);
 
         _body.material = _skinVariants[i].BodyMaterial;
@@ -30,4 +63,10 @@ public struct SkinVariant
 {
     public Material BodyMaterial;
     public Material[] EarVariants, TailVariants; 
+}
+
+[System.Serializable]
+public struct FaceVariant
+{
+    public Sprite NeutralFace, HappyFace, MadFace, DisgustFace, ChewingFace;
 }
