cdf7091134fb85ced70f06b4ff8d83078b576b6e
SF-23 <angelitojoseph.astilla@benilde.edu.ph>
2025-03-14 20:10:22 +0800

    Started work on Restaurant Review Receipt
    Receipt currently is in UI Test scene
    - Added The BG, the texts and created the prefabs for the letters we'll be rating the player
    - Prefabs already listed inside an array for the script, RestaurantReceipt

---
+++ OP_ChopChop!/Assets/_Scripts/UI/RestaurantReceipt.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestaurantReceipt : MonoBehaviour
+{
+    [SerializeField] private GameObject[] customerRatings;
+    [SerializeField] private GameObject[] kicthenRatings;
+    [SerializeField] private GameObject[] restaurantRatings;
+
+
+}
