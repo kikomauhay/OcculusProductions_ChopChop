edb1156e8c17283ac85f740b851fe2bdea632c12
AJ <angelitojoseph.astilla@benilde.edu.ph>
2025-03-15 23:41:15 +0800

    UI stuff
    - Finished End of day Receipt Script and UI
    - Created new OrderUI for all orders, Nigiri and Sashimi orders
    
    QoL:
    - Created folders for the different UI prefabs we have for easy search and access

--- OP_ChopChop!/Assets/_Scripts/UI/RestaurantReceipt.cs
+++ OP_ChopChop!/Assets/_Scripts/UI/RestaurantReceipt.cs
@@ -8,5 +8,22 @@ public class RestaurantReceipt : MonoBehaviour
     [SerializeField] private GameObject[] kicthenRatings;
     [SerializeField] private GameObject[] restaurantRatings;
 
+    //index is set according to order of rating which is S to F.
+    //0 = S....4 = F.
+
+    private void GiveCustomerRating(int index)  
+    {
+        customerRatings[index].gameObject.SetActive(true);
+    }
+
+    private void GiveKitchenRating(int index)
+    {
+        kicthenRatings[index].gameObject.SetActive(true);
+    }
+
+    private void GiveRestaurantRating(int index)
+    {
+        restaurantRatings[index].gameObject.SetActive(true);
+    }
 
 }
