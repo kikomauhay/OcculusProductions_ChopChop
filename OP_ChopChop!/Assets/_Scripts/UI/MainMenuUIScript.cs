b7c0676f0dc9533f8a092bc413db72a07a431e29
AJ <angelitojoseph.astilla@benilde.edu.ph>
2025-03-22 01:19:57 +0800

    Updated MainMenuUI_Prefab
    - Added MainMenuUI Script, it's a singleton
    - Added Function in this script to toggle the various elements in it
    - Added buttons and assigned functions accordingly to control what elements appear on screen
    - Added a live wallpaper, namedScreenSaver (reduced saturation so it isnt to attracting to look at)

---
+++ OP_ChopChop!/Assets/_Scripts/UI/MainMenuUIScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIScript : Singleton<MainMenuUIScript>
+{
+    [SerializeField] private GameObject playIcon;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject settingsPanel;
+
+    [SerializeField] private Slider masterSlider;  //We need to fix this slider for the volumeeeeee
+
+    protected override void Awake() => base.Awake();
+    protected override void OnApplicationQuit() => base.OnApplicationQuit();
+
+    public void TogglePlayIcon(bool toggle)
+    {
+        playIcon.SetActive(toggle);
+    }
+
+    public void TogglePausePanel(bool isTrue)
+    {
+        pausePanel.SetActive(isTrue);
+    }
+
+    public void ExitSettingsPanel()
+    {
+        settingsPanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    public void ToggleSettingsPanel(bool isTrue)
+    {
+        settingsPanel.SetActive(isTrue);
+        pausePanel.SetActive(false);
+    }
+}
