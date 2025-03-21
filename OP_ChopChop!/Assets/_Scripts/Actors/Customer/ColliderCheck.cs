8ad26d841fc8ca062b233331e5025034ebb28776
Mauhay <12107793@benilde.edu.ph>
2025-03-21 16:12:19 +0800

    Bug Fixes

--- OP_ChopChop!/Assets/_Scripts/Actors/Customer/ColliderCheck.cs
+++ OP_ChopChop!/Assets/_Scripts/Actors/Customer/ColliderCheck.cs
@@ -7,16 +7,16 @@ public class ColliderCheck : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (CustomerOrder == null) 
+        if (CustomerOrder == null)
         {
             Debug.LogError("Null CustomerOrder");
             return;
         }
 
-        if (other.gameObject.GetComponent<Ingredient>() != null) 
+        if (other.gameObject.GetComponent<Ingredient>() != null)
         {
             Debug.LogError("GIVEN ORDER IS AN INGREDIENT");
-            
+
             // CustomerOrder.StartCoroutine("DoNegativeReaction");
             StartCoroutine(CustomerOrder.DoReaction(FaceVariant.MAD));
 
@@ -33,10 +33,10 @@ public class ColliderCheck : MonoBehaviour
 
             // CustomerOrder.StartCoroutine("DoPositiveReaction");
             StartCoroutine(CustomerOrder.DoReaction(FaceVariant.HAPPY));
-            
+
             // calculates the customer SR
             CustomerOrder.CustomerSR = (collidedDish.DishScore + CustomerOrder.PatienceRate) / 2f;
-            
+
             // customer "eats" the food
             Destroy(collidedDish.gameObject);
             // SpawnManager.Instance.RemoveCustomer(CustomerOrder.gameObject);
@@ -51,9 +51,11 @@ public class ColliderCheck : MonoBehaviour
         Debug.LogError("WRONG ORDER");
 
         // CustomerOrder.StartCoroutine("DoNegativeReaction");
-        StartCoroutine(CustomerOrder.DoReaction(FaceVariant.MAD));
+        /*        StartCoroutine(CustomerOrder.DoReaction(FaceVariant.MAD));
 
-        Destroy(other.gameObject);
+                Destroy(other.gameObject);*/
+
+        //Create restriction on the code above, it also destroys the players hand on check
     }
 
     IEnumerator DisableColider()
