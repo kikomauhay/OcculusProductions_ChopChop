f8b12854d2bf68a92eba21f6188dea90fd0ce34e
Isagani Coel <coelfactora@gmail.com>
2025-04-26 00:17:21 +0800

    0.41.2
    - added bubble vfx

--- OP_ChopChop!/Assets/_Scripts/Actors/Water.cs
+++ OP_ChopChop!/Assets/_Scripts/Actors/Water.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
+#region Members
+ 
     [SerializeField] private bool _isTutorial;
+    [SerializeField] private float _cooldownTimer, _cooldownInterval; 
+    [SerializeField] private GameObject _bubbleVFX;
+
+#endregion
+
+#region Unity
 
     private void OnEnable() => SoundManager.Instance.PlaySound("tap water", SoundGroup.APPLIANCES);
-    private void OnDisable() => SoundManager.Instance.SoundSource.Stop();    
-
+    private void OnDisable() => SoundManager.Instance.SoundSource.Stop();
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Sponge>() != null)
@@ -14,17 +22,50 @@ public class Water : MonoBehaviour
             
         if (other.gameObject.GetComponent<HandWashing>() != null)
         {
-            if (_isTutorial)
-            {
-                transform.parent.GetComponentInChildren<OutlineMaterial>().DisableHighlight();
-                OnBoardingHandler.Instance.CallOnboarding(1);
-                return;
-            }
-            
             HandWashing handWash = other.gameObject.GetComponent<HandWashing>();
 
             if (!handWash.IsWet)
                 handWash.ToggleWet();
         }
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!_isTutorial) return;
+
+        if (other.gameObject.GetComponent<HandWashing>() == null) return;
+
+        // spanwning bubbles at an interval
+        _cooldownTimer -= Time.deltaTime;
+        if (_cooldownTimer <= 0f)
+        {
+            SpawnBubbles(other.gameObject.transform);
+            _cooldownTimer = _cooldownInterval;
+        }
+    }    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<HandWashing>() == null) return;
+
+        if (_isTutorial)
+        {
+            transform.parent.GetComponentInChildren<OutlineMaterial>().DisableHighlight();
+            OnBoardingHandler.Instance.CallOnboarding(1);
+        }
+    }
+
+#endregion
+
+#region Helpers
+
+    private void SpawnBubbles(Transform t)
+    {
+        GameObject newVFX = Instantiate(_bubbleVFX, 
+                                        t.transform.position,
+                                        t.transform.rotation,
+                                        transform);
+
+        Destroy(newVFX, _cooldownInterval);
+    }
+
+#endregion
 }
